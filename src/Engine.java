027e2992159fa4782f5d9898f9f05f04ded5c694
Intesar-Tahmid <intesar3006@gmail.com>
2023-08-22 00:06:28 +0600

    Implemented Engine Class

--- src/Engine.java
+++ src/Engine.java
@@ -1,2 +1,85 @@
-public class Engine {
+// The Engine interface (Product)
+public interface Engine {
+    double EnginePrice();
+    String TypeofEngine();
 }
+
+// Concrete implementations of the Engine
+class CC1300 implements Engine {
+    @Override
+    public String TypeofEngine() {
+        return "CC1300";
+    }
+
+    @Override
+    public double EnginePrice() {
+        return 100000;
+    }
+}
+
+class CC1700 implements Engine {
+    @Override
+    public String TypeofEngine() {
+        return "CC1700";
+    }
+
+    @Override
+    public double EnginePrice() {
+        return 200000;
+    }
+}
+
+class CC1800 implements Engine {
+    @Override
+    public String TypeofEngine() {
+        return "CC1800";
+    }
+
+    @Override
+    public double EnginePrice() {
+        return 300000;
+    }
+}
+
+class CC2100 implements Engine {
+    @Override
+    public String TypeofEngine() {
+        return "CC2100";
+    }
+
+    @Override
+    public double EnginePrice() {
+        return 400000;
+    }
+}
+
+// Concrete factories
+class CC1300Factory implements EngineFactory {
+    @Override
+    public Engine createEngine() {
+        return new CC1300();
+    }
+}
+
+class CC1700Factory implements EngineFactory {
+    @Override
+    public Engine createEngine() {
+        return new CC1700();
+    }
+}
+
+class CC1800Factory implements EngineFactory {
+    @Override
+    public Engine createEngine() {
+        return new CC1800();
+    }
+}
+
+class CC2100Factory implements EngineFactory {
+    @Override
+    public Engine createEngine() {
+        return new CC2100();
+    }
+}
+
+
---
+++ src/EngineFactory.java
@@ -0,0 +1,4 @@
+// The EngineFactory interface (Creator)
+public interface EngineFactory {
+    Engine createEngine();
+}
